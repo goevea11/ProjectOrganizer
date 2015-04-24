9587648c589c86ea5c9fdb1d9e283d8be0d98c3d
goevea11 <goevea11@htlkaindorf.at>
2015-04-24 08:31:48 +0200

    DBAccess

--- src/database/DBAccess.java
+++ src/database/DBAccess.java
@@ -1,25 +1,24 @@
 package database;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
 /**
  *
  * @author Veronika
  */
-public class DBAccess {
-    
-    
-    
-    public static void main(String[] args) {
-        
+public class DBAccess
+{
+   private Database db;
+   public static void main(String[] args) {
+       
+   }
+  /** Creates a new instance of DBAccess */
+   public DBAccess(String dbname)
+            throws IOException, FileNotFoundException, 
+            ClassNotFoundException, SQLException {
+        db = Database.getInstance();
+        db.connect(dbname);
     }
-    
-    
-    public String checkAccExistence(int username, String password){
-        return "toimplement";
-    }
-    
-    
-    public void createAccount(String vorname,String nachname,String geburtsdatum,  String password ){
-        
-    }
-    
 }
