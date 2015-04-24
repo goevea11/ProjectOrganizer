68afdbde1012c4fdd62789ab20667d29e56b5cd0
goevea11 <goevea11@htlkaindorf.at>
2015-04-24 08:32:16 +0200

    DBAccess

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,3 +1,4 @@
+
 package database;
 
 import java.io.File;
@@ -9,60 +10,55 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-/**
- *
- * @author Veronika
- */
+
 public class Database {
-    //Connection zur Datenbank aufbauen
-    //SINGLETON
-    //DB_Url+dbname, DB_Username, DB_Password
-
-    private String DB_Driver = "";
-    private String DB_Username = "";
-    private String DB_Password = "";
-    private String DB_Url = "";
-    private Connection con;
-    private final String filename = System.getProperty("user.dir") + File.separator + "src" + File.separator + "database" + File.separator + "dbproperties.properties";
-    public static Database instance;
-
     public Connection getCon() {
         return con;
     }
-
-    ;
-
-  public static Database getInstance() throws FileNotFoundException, IOException, ClassNotFoundException {
-
-        if (instance == null) {
-            instance = new Database();
+    private String DB_Driver="";
+    private String DB_Username="";
+    private String DB_Password="";
+    private String DB_Url="";
+    Connection con;
+    
+    private final String fileName=System.getProperty("user.dir")
+            +File.separator+"src"+File.separator+"data"+
+            File.separator + "dbproperties.properties";
+    // connect
+    // dissconnect
+    private static Database theInstance = null;
+    
+    public static Database getInstance() 
+            throws FileNotFoundException, IOException, ClassNotFoundException
+    {
+        if(theInstance==null)
+        {
+            theInstance = new Database();
         }
-        return instance;
+        return theInstance;
     }
-
-    private Database() throws FileNotFoundException, IOException, ClassNotFoundException {
+    private Database() 
+            throws FileNotFoundException, IOException, ClassNotFoundException
+    {
         loadProperties();
         Class.forName(this.DB_Driver);
-
     }
-
-    public void connect(String dbname) throws SQLException {
-        con = DriverManager.getConnection(DB_Url + dbname, this.DB_Username, this.DB_Password);
+    public void connect(String dbname) throws SQLException
+    {
+        con=DriverManager.getConnection(DB_Url+dbname,DB_Username, DB_Password);
     }
-
-    public void disconnect() throws SQLException {
+    public void disconnect() throws SQLException
+    {
         con.close();
     }
-
-    private void loadProperties() throws FileNotFoundException, IOException {
-        Properties prop = new Properties();
-        FileInputStream in = new FileInputStream(filename);
-        prop.load(in);
-        DB_Driver = prop.getProperty("driver");
-        DB_Username = prop.getProperty("username");
-        DB_Password = prop.getProperty("password");
-        DB_Url = prop.getProperty("url");
-
-
+    private void loadProperties() throws FileNotFoundException, IOException
+    {
+        Properties props = new Properties();
+        FileInputStream in = new FileInputStream(fileName);
+        props.load(in);
+        DB_Driver = props.getProperty("driver");
+        DB_Username=props.getProperty("username");
+        DB_Password=props.getProperty("password");
+        DB_Url = props.getProperty("url");
     }
 }
