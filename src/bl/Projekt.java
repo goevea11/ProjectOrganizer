d3a4fa3f645400ce32660b89533e52c6b5fb314f
zeidoa11 <d.zeiler7@gmail.com>
2015-04-27 14:15:42 +0200

    InsertMitarbeiterDialog

---
+++ src/bl/Projekt.java
@@ -0,0 +1,53 @@
+
+package bl;
+
+import java.util.Date;
+
+public class Projekt {
+    private int projektid;
+    private String name;
+    private Date anfangsdatum,enddatum;
+
+    public Projekt(int projektid, String name, Date anfangsdatum, Date enddatum) {
+        this.projektid = projektid;
+        this.name = name;
+        this.anfangsdatum = anfangsdatum;
+        this.enddatum = enddatum;
+    }
+
+    public int getProjektid() {
+        return projektid;
+    }
+
+    public void setProjektid(int projektid) {
+        this.projektid = projektid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getAnfangsdatum() {
+        return anfangsdatum;
+    }
+
+    public void setAnfangsdatum(Date anfangsdatum) {
+        this.anfangsdatum = anfangsdatum;
+    }
+
+    public Date getEnddatum() {
+        return enddatum;
+    }
+
+    public void setEnddatum(Date enddatum) {
+        this.enddatum = enddatum;
+    }
+    
+    
+    
+    
+}
