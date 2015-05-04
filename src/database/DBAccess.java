69e6d9aeb14b4ef54ef75682e0c6ef12c9fd8dbc
zeidoa11 <d.zeiler7@gmail.com>
2015-05-04 13:32:46 +0200

    get Projekte

--- src/database/DBAccess.java
+++ src/database/DBAccess.java
@@ -1,11 +1,13 @@
 package database;
 
 import bl.Mitarbeiter;
+import bl.Projekt;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
@@ -89,8 +91,10 @@ public class DBAccess {
             String sqlString = "SElECT nachname FROM \"Mitarbeiter\" WHERE passwort='" + passwort + "' AND nachname='" + nachname + "';";
             ResultSet rs = stat.executeQuery(sqlString);
 
-            rs.next();
-            returnname = rs.getString(1);
+            if(rs.next()){
+                returnname = rs.getString(1); 
+            }
+           
             stat.close();
 
         } catch (SQLException ex) {
@@ -101,6 +105,55 @@ public class DBAccess {
         }
         return returnname;
     }
+
+
+public void insertProjekt(Projekt p, Mitarbeiter gründer){
+    
+     try {
+           
+           SimpleDateFormat  sdf = new SimpleDateFormat("dd.MM.yyyy");
+          
+           Statement stat = db.getCon().createStatement();
+           
+           
+           //Projekt einfügen
+           String sqlString="INSERT INTO \"Projekt\"(\n" +
+"            Projectid, Name, anfangsdatum, enddatum)\n" +
+"    VALUES ((SELECT MAX(Projectid) FROM \"Projekt\")+1, '"+p.getName()+"', TO_DATE('"+sdf.format(p.getAnfangsdatum())+"','dd.MM.yyyy'),TO_DATE('"+sdf.format(p.getAnfangsdatum())+"','dd.MM.yyyy'));";
+           stat.executeUpdate(sqlString);
+           
+           //Verbindung zwischen Mitarbeiter und Projektersteller hersteller
+           sqlString="INSERT INTO Mitarbeiter";
+           stat.close();
+       } catch (SQLException ex) {
+           Logger.getLogger(DBAccess.class.getName()).log(Level.SEVERE, null, ex);
+       }
+}
+
+
+public LinkedList<Projekt> getProjekte(String mitarbeiterid){
+    
+    LinkedList<Projekt> projekte=new LinkedList<Projekt>();
+    //Zuerst Mitarbeitet, MitarbeiterProject und Project joinen
+  try {
+            Statement stat = db.getCon().createStatement();
+            String sqlString = "SELECT Projectid, Name, anfangsdatum, enddatum "
+                    + "FROM \"Mitarbeiter\" m INNER JOIN \"MitarbeiterProject\" mp ON(m.mitarbeiterid==mp.mitarbeiterid)"
+                    + " INNER JOIN Project p ON (p.Projectid==mp.projectid)"
+            + "WHERE m.mitarbeiterid='"+mitarbeiterid+"';";
+            ResultSet rs = stat.executeQuery(sqlString);
+ SimpleDateFormat  sdf = new SimpleDateFormat("dd.MM.yyyy");
+            while (rs.next()) {
+              projekte.add(new Projekt(rs.getInt(1),rs.getString(2),sdf.parse(rs.getDate(3).toString()),sdf.parse(rs.getDate(4).toString())));
+            }
+            stat.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBAccess.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+           Logger.getLogger(DBAccess.class.getName()).log(Level.SEVERE, null, ex);
+       }
+     return projekte;
+}
 }
 
     
