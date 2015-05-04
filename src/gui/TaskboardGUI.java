cc679c322aae9ec1754ded35fe93076b9a9277f9
goevea11 <goevea11@htlkaindorf.at>
2015-05-04 14:43:35 +0200

    Taskboard GUI

--- src/gui/TaskboardGUI.java
+++ src/gui/TaskboardGUI.java
@@ -5,6 +5,8 @@
  */
 package gui;
 
+import javax.swing.DefaultListModel;
+
 /**
  *
  * @author Veronika
@@ -12,10 +14,22 @@ package gui;
 public class TaskboardGUI extends javax.swing.JFrame {
 
     /**
+     * 
      * Creates new form Taskboard
      */
+    
+    private DefaultListModel todomodel;
+    private DefaultListModel inworkmodel;
+    private DefaultListModel finishedmodel;
+    
     public TaskboardGUI() {
         initComponents();
+        todomodel = new DefaultListModel();
+        this.todoList.setModel(todomodel);
+        inworkmodel = new DefaultListModel();
+        this.inworkList.setModel(inworkmodel);
+        finishedmodel = new DefaultListModel();
+        this.finishedList.setModel(finishedmodel);
     }
 
     /**
@@ -30,19 +44,19 @@ public class TaskboardGUI extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList();
+        todoList = new javax.swing.JList();
         jPanel2 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        todo_inwork_right = new javax.swing.JButton();
+        todo_inwork_left = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jList2 = new javax.swing.JList();
+        inworkList = new javax.swing.JList();
         jPanel4 = new javax.swing.JPanel();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        inwork_finished_right = new javax.swing.JButton();
+        inwork_finished_left = new javax.swing.JButton();
         jPanel5 = new javax.swing.JPanel();
         jScrollPane3 = new javax.swing.JScrollPane();
-        jList3 = new javax.swing.JList();
+        finishedList = new javax.swing.JList();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new java.awt.GridLayout(1, 5));
@@ -51,12 +65,12 @@ public class TaskboardGUI extends javax.swing.JFrame {
 
         jScrollPane1.setBorder(javax.swing.BorderFactory.createTitledBorder("TO DO"));
 
-        jList1.setModel(new javax.swing.AbstractListModel() {
+        todoList.setModel(new javax.swing.AbstractListModel() {
             String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
             public int getSize() { return strings.length; }
             public Object getElementAt(int i) { return strings[i]; }
         });
-        jScrollPane1.setViewportView(jList1);
+        jScrollPane1.setViewportView(todoList);
 
         jPanel1.add(jScrollPane1, java.awt.BorderLayout.CENTER);
 
@@ -64,14 +78,24 @@ public class TaskboardGUI extends javax.swing.JFrame {
 
         jPanel2.setLayout(new java.awt.GridBagLayout());
 
-        jButton1.setText("->");
-        jPanel2.add(jButton1, new java.awt.GridBagConstraints());
+        todo_inwork_right.setText("->");
+        todo_inwork_right.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                todo_inwork_rightActionPerformed(evt);
+            }
+        });
+        jPanel2.add(todo_inwork_right, new java.awt.GridBagConstraints());
 
-        jButton2.setText("<-");
+        todo_inwork_left.setText("<-");
+        todo_inwork_left.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                todo_inwork_leftActionPerformed(evt);
+            }
+        });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
-        jPanel2.add(jButton2, gridBagConstraints);
+        jPanel2.add(todo_inwork_left, gridBagConstraints);
 
         getContentPane().add(jPanel2);
 
@@ -79,12 +103,12 @@ public class TaskboardGUI extends javax.swing.JFrame {
 
         jScrollPane2.setBorder(javax.swing.BorderFactory.createTitledBorder("IN ARBEIT"));
 
-        jList2.setModel(new javax.swing.AbstractListModel() {
+        inworkList.setModel(new javax.swing.AbstractListModel() {
             String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
             public int getSize() { return strings.length; }
             public Object getElementAt(int i) { return strings[i]; }
         });
-        jScrollPane2.setViewportView(jList2);
+        jScrollPane2.setViewportView(inworkList);
 
         jPanel3.add(jScrollPane2, java.awt.BorderLayout.CENTER);
 
@@ -92,14 +116,24 @@ public class TaskboardGUI extends javax.swing.JFrame {
 
         jPanel4.setLayout(new java.awt.GridBagLayout());
 
-        jButton3.setText("->");
-        jPanel4.add(jButton3, new java.awt.GridBagConstraints());
+        inwork_finished_right.setText("->");
+        inwork_finished_right.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                inwork_finished_rightActionPerformed(evt);
+            }
+        });
+        jPanel4.add(inwork_finished_right, new java.awt.GridBagConstraints());
 
-        jButton4.setText("<-");
+        inwork_finished_left.setText("<-");
+        inwork_finished_left.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                inwork_finished_leftActionPerformed(evt);
+            }
+        });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
-        jPanel4.add(jButton4, gridBagConstraints);
+        jPanel4.add(inwork_finished_left, gridBagConstraints);
 
         getContentPane().add(jPanel4);
 
@@ -107,12 +141,12 @@ public class TaskboardGUI extends javax.swing.JFrame {
 
         jScrollPane3.setBorder(javax.swing.BorderFactory.createTitledBorder("FERTIG"));
 
-        jList3.setModel(new javax.swing.AbstractListModel() {
+        finishedList.setModel(new javax.swing.AbstractListModel() {
             String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
             public int getSize() { return strings.length; }
             public Object getElementAt(int i) { return strings[i]; }
         });
-        jScrollPane3.setViewportView(jList3);
+        jScrollPane3.setViewportView(finishedList);
 
         jPanel5.add(jScrollPane3, java.awt.BorderLayout.CENTER);
 
@@ -120,6 +154,22 @@ public class TaskboardGUI extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void todo_inwork_rightActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_todo_inwork_rightActionPerformed
+        
+    }//GEN-LAST:event_todo_inwork_rightActionPerformed
+
+    private void todo_inwork_leftActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_todo_inwork_leftActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_todo_inwork_leftActionPerformed
+
+    private void inwork_finished_rightActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inwork_finished_rightActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_inwork_finished_rightActionPerformed
+
+    private void inwork_finished_leftActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inwork_finished_leftActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_inwork_finished_leftActionPerformed
 
     /**
      * @param args the command line arguments
@@ -158,13 +208,10 @@ public class TaskboardGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JList jList1;
-    private javax.swing.JList jList2;
-    private javax.swing.JList jList3;
+    private javax.swing.JList finishedList;
+    private javax.swing.JList inworkList;
+    private javax.swing.JButton inwork_finished_left;
+    private javax.swing.JButton inwork_finished_right;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
@@ -173,5 +220,8 @@ public class TaskboardGUI extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JList todoList;
+    private javax.swing.JButton todo_inwork_left;
+    private javax.swing.JButton todo_inwork_right;
     // End of variables declaration//GEN-END:variables
 }
